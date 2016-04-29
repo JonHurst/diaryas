f470eb6d0a56b765d235ea0a90249edffa3b9e17
Jon Hurst <jon@hursts.org.uk>
2016-04-29 12:40:35 +0100

    Added module to escape a string so that it is suitable for latex processing

---
+++ latex_escape.py
@@ -0,0 +1,33 @@
+def latex_escape(str):
+    subs = {"$": r"\$",
+            "&": r"\&",
+            "%": r"\%",
+            "#": r"\#",
+            "_": r"\_",
+            "{": r"\{",
+            "}": r"\}",
+            "~": r"$\sim$",
+            "^": r"$\wedge$",
+            "\\": r"$\backslash$",
+            "<": "$<$",
+            ">": "$>$"}
+    outstr = ""
+    open_double_quote = True
+    for c in str:
+        if c in subs:
+            outstr += subs[c]
+        elif c == "\"":
+            if open_double_quote:
+                open_double_quote = False
+                outstr += "``"
+            else:
+                open_double_quote = True
+                outstr += "''"
+        else:
+            outstr += c
+    return outstr
+
+if __name__ == "__main__":
+    testcase = r"$trouble$%ahead#with{this}~^\statement<>"
+    print(testcase)
+    print(latex_escape(testcase))
