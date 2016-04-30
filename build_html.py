d1fc156a76a72b0fa9fa10f1ac3e95b844785596
Jon Hurst <jon@hursts.org.uk>
2016-04-30 09:06:05 +0100

    Changed _template to _t

--- build_html.py
+++ build_html.py
@@ -2,7 +2,7 @@ import string
 import re
 import html
 
-main_template = string.Template("""\
+main_t = string.Template("""\
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -20,21 +20,21 @@ $body
 </html>
 """)
 
-entryblock_template = string.Template("""\
+entryblock_t = string.Template("""\
 <h2 class='$type'>$date</h2>
 $entry_body
 """)
 
-entrybody_template = string.Template("""\
+entrybody_t = string.Template("""\
 <ul class='events'>
 $entries</ul>
 """)
 
-timed_template = string.Template("""\
+timed_t = string.Template("""\
 <span class='time'>$timestring</span> $description
 """)
 
-entrystring_template = string.Template("""\
+entrystring_t = string.Template("""\
 <li class='$type'>$description</li>
 """)
 
@@ -47,14 +47,14 @@ def build_html(diary, startdate, enddate):
         if d[0] > enddate: break
         entries = ""
         for e in d[1]:
-            entries += entrystring_template.substitute(type="holiday",
+            entries += entrystring_t.substitute(type="holiday",
                                                        description=html.escape(e))
         for e in d[2:]:
             e = html.escape(e)
             mo = reo_entry.match(e)
             entry_type = "entry"
             if mo:
-                e = timed_template.substitute(timestring=mo.group(1),
+                e = timed_t.substitute(timestring=mo.group(1),
                                               description=mo.group(2))
                 entry_type = "timed"
             elif e == "*School*":
@@ -69,12 +69,12 @@ def build_html(diary, startdate, enddate):
             elif e[:3] == "xxx" and e[-3:] == "xxx":
                 e = e[3:-3]
                 entry_type = "strike"
-            entries += entrystring_template.substitute(type=entry_type,
+            entries += entrystring_t.substitute(type=entry_type,
                                                        description=e)
         entrybody = ""
         if len(entries) != 0:
-            entrybody = entrybody_template.substitute(entries=entries)
-        body += entryblock_template.substitute(date = d[0].strftime("%A, %d/%m/%Y"),
+            entrybody = entrybody_t.substitute(entries=entries)
+        body += entryblock_t.substitute(date = d[0].strftime("%A, %d/%m/%Y"),
                                                type="day " + d[0].strftime("%a").lower(),
                                                entry_body=entrybody)
-    return main_template.substitute(body=body)
+    return main_t.substitute(body=body)
