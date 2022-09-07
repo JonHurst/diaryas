b0bcdb4f5daff1b318f26b3feb0c011a6c2ca885
Jon Hurst <jon.a@hursts.org.uk>
2022-09-07 11:02:38 +0100

    Improve html output

--- build_html.py
+++ build_html.py
@@ -33,8 +33,7 @@ $body
 entryblock_t = string.Template("""\
 <div class="org-day" id='d$isodate'>
 <h2 class='$type'>$date</h2>
-<ul class='org-eventlist'>
-$entry_body</ul>
+$body
 </div>
 """)
 
@@ -62,31 +61,45 @@ def build_tags(taglist):
 def build_html(diary: Tuple[DayRecord]) -> str:
     body = "<div class='org-week'>\n"
     for rec in diary:
-        entries = ""
-        for hol in rec.holidays:
-            entries += (
-                "<li class='org-eventlist__event "
-                + "org-eventlist__event--holiday'>"
-                + html.escape(hol)
-                + "</li>\n")
+        day_body = []
         if rec.tags:
             html_tags = build_tags(rec.tags)
-            entries += (
-                '<li class="org-eventlist__taglist"><div class="org-tags">\n'
-                + '\n'.join(html_tags)
-                + "\n</div></li>\n")
+            day_body.append(
+                '<div class="org-tags">\n'
+                + ''.join(html_tags)
+                + "\n</div>")
+        if rec.holidays:
+            entries = ""
+            for hol in rec.holidays:
+                entries += (
+                    "<li class='org-holidays__entry'>"
+                    + html.escape(hol)
+                    + "</li>\n")
+            day_body.append(
+                "<ul class='org-holidays'>\n"
+                + entries
+                + "</ul>")
+        timed_events, untimed_events = [], []
         for e in rec.entries:
-            timestr = (
-                f"<span class='org-time'>{html.escape(e.timestr)}</span> "
-                if e.timestr else "")
-            entry_type = (
-                "org-eventlist__event--timed"
-                if e.timestr else "org-eventlist__event--untimed")
             text = html.escape(e.text)
             text = text.replace("\n", " <br/>")
             text = process_strikethrough(text)
-            entries += (f"<li class='org-eventlist__event {entry_type}'>"
-                        f"{timestr}{text}</li>\n")
+            if e.timestr:
+                timestr = (f"<span class='org-time'>"
+                           f"{html.escape(e.timestr)}</span> ")
+                timed_events.append(
+                    f"<li class='org-timed__event'>"
+                    f"{timestr}{text}</li>")
+            else:
+                untimed_events.append(
+                    f"<li class='org-untimed__event'>"
+                    f"{text}</li>")
+        if untimed_events:
+            untimed_body = "\n".join(untimed_events)
+            day_body.append(f"<ul class='org-untimed'>\n{untimed_body}\n</ul>")
+        if timed_events:
+            timed_body = "\n".join(timed_events)
+            day_body.append(f"<ul class='org-timed'>\n{timed_body}\n</ul>")
         day = rec.date.strftime("%a").lower()
         body += entryblock_t.substitute(
             date=rec.date.strftime("%A, %d/%m/%Y"),
@@ -94,7 +107,7 @@ def build_html(diary: Tuple[DayRecord]) -> str:
             type=("org-heading "
                   + ("org-heading--weekend" if day in ["sat", "sun"]
                      else "org-heading--weekday")),
-            entry_body=entries)
+            body="\n".join(day_body))
         if day == "sun":
             body += "</div><div class='org-week'>\n"
     body = body[:-len("<div class='org-week'>\n")]
